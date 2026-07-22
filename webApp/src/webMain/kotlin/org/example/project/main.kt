d395319c500f1c4b2f692b7940d6b7f87f65455b
ankit <ankitkumar@apptunix.com>
2026-07-22 11:00:06 +0530

    added PWA for website

--- webApp/src/webMain/kotlin/org/example/project/main.kt
+++ webApp/src/webMain/kotlin/org/example/project/main.kt
@@ -4,9 +4,11 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import org.example.project.di.*
 import org.koin.core.context.startKoin
+import kotlin.js.js
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    registerServiceWorker()
     startKoin {
         modules(
             webDatabaseModule,
@@ -20,8 +22,18 @@ fun main() {
     }
 
     ComposeViewport(viewportContainerId = "app", configure = {
-
     }) {
         App()
     }
+}
+
+
+fun registerServiceWorker() {
+    js(
+        """
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/sw.js');
+        }
+        """
+    )
 }
---
+++ webApp/src/webMain/resources/sw.js
@@ -0,0 +1,20 @@
+const CACHE = "tracker-v1";
+
+const FILES = [
+    "/",
+    "/index.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
